903e7722ce49955e45ceb84f05d845b3e8118c6f
bumsoo <meliketoy@gmail.com>
2016-10-13 14:35:51 +0900

    Modified .gitignore list

---
+++ requirements.sh
@@ -0,0 +1,8 @@
+mkdir preprocessedData/
+mkdir preprocessedData/train/
+mkdir preprocessedData/val/
+mkdir preprocessedData/train/benign
+mkdir preprocessedData/train/malignant
+mkdir preprocessedData/val/benign
+mkdir preprocessedData/val/malignant
+
