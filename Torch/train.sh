24ea08b65a46d50f7c2156b508f2be5ade0d917d
bumsoo <meliketoy@gmail.com>
2016-12-09 03:29:15 +0900

    mod train script

--- Torch/train.sh
+++ Torch/train.sh
@@ -1,24 +1,26 @@
-export netType='resnet'
-export depth=50
+export netType='wide-resnet'
+export depth=10
+export width=1
 export dataset='dreamChallenge'
 export data='preprocessedData'
-export save=logs/${netType}-${depth}
+export save=logs/${netType}-${depth}x${width}/
 export experiment_number=1
 mkdir -p $save
 mkdir -p modelState
 mkdir -p scratch
 
 th main.lua \
--testPhase false \
 -dataset ${dataset} \
 -data ${data} \
 -netType ${netType} \
 -nGPU 2 \
 -batchSize 32 \
 -dropout 0.3 \
+-imageSize 512 \
+-cropSize 512 \
 -top5_display false \
 -testOnly false \
 -resume '' \
 -depth ${depth} \
--widen_factor 2 \
+-widen_factor ${width} \
 # | tee $save/train_log${experiment_number}.txt
