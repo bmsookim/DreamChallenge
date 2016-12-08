98d449b9060887fd4e709bc808fcee93e42415e9
bumsoo <meliketoy@gmail.com>
2016-12-09 03:38:51 +0900

    Added size options

--- Torch/test.sh
+++ Torch/test.sh
@@ -1,17 +1,19 @@
 export netType='wide-resnet'
+export depth=10
+export width=1
 export dataset='dreamChallenge'
 export data='preprocessedData'
 
-th main.lua \
+th test.lua \
 -dataset ${dataset} \
 -data ${data} \
 -netType ${netType} \
 -nGPU 2 \
 -batchSize 32 \
--dropout 0.5 \
--resume 'scratch' \
--testPhase true \
+-dropout 0 \
+-imageSize 512 \
+-cropSize 512 \
 -top5_display false \
--depth 28 \
--widen_factor 10
+-depth ${depth} \
+-widen_factor ${width}
 
