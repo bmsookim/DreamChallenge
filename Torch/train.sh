4edef72dd301f36cfa5b57af75b86d006a447a6e
bumsoo <meliketoy@gmail.com>
2016-12-10 23:14:57 +0900

    training script

--- Torch/train.sh
+++ Torch/train.sh
@@ -2,9 +2,10 @@ export netType='wide-resnet'
 export depth=10
 export width=1
 export dataset='dreamChallenge'
-export data='preprocessedData'
+export data='/preprocessedData/dreamCh/'
 export save=logs/${netType}-${depth}x${width}/
 export experiment_number=1
+export CUDA_VISIBLE_DEVICES="0,1"
 mkdir -p $save
 mkdir -p modelState
 mkdir -p scratch
@@ -15,9 +16,9 @@ th main.lua \
 -netType ${netType} \
 -nGPU 2 \
 -batchSize 32 \
--dropout 0.3 \
--imageSize 512 \
--cropSize 512 \
+-dropout 0 \
+-imageSize 1024 \
+-cropSize 1024 \
 -top5_display false \
 -testOnly false \
 -resume '' \
