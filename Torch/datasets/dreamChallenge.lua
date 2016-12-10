393c2a661900c013bec3df93c2afa45551e898fe
bumsoo <meliketoy@gmail.com>
2016-12-10 23:13:56 +0900

    data loading format

--- Torch/datasets/dreamChallenge.lua
+++ Torch/datasets/dreamChallenge.lua
@@ -63,32 +63,11 @@ function ImagenetDataset:size()
    return self.imageInfo.imageClass:size(1)
 end
 
--- Computed from random subset of ImageNet training images
-local meanstd = {
-   mean = { 0.485, 0.456, 0.406 },
-   std = { 0.229, 0.224, 0.225 },
-}
-local pca = {
-   eigval = torch.Tensor{ 0.2175, 0.0188, 0.0045 },
-   eigvec = torch.Tensor{
-      { -0.5675,  0.7192,  0.4009 },
-      { -0.5808, -0.0045, -0.8140 },
-      { -0.5836, -0.6948,  0.4203 },
-   },
-}
-
 function ImagenetDataset:preprocess()
    if self.split == 'train' then
       return t.Compose{
          t.RandomSizedCrop(self.opt.cropSize),
-         t.ColorJitter({
-            brightness = 0.4,
-            contrast = 0.4,
-            saturation = 0.4,
-         }),
-         t.Lighting(0.1, pca.eigval, pca.eigvec),
          t.ColorNormalize(meanstd),
-         t.HorizontalFlip(0.5),
       }
    elseif self.split == 'val' then
       local Crop = self.opt.tenCrop and t.TenCrop or t.CenterCrop
