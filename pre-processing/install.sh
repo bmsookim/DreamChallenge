3285504d881bb336c8db1414342b90367626afea
MinhwanYu - zeus <minhwan90@gmail.com>
2016-12-11 22:52:13 +0900

    separate  and

--- pre-processing/install.sh
+++ pre-processing/install.sh
@@ -2,38 +2,19 @@
 
 WORKSPACE=$PWD
 
+apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler -y
+apt-get install --no-install-recommends libboost-all-dev -y
+
 # clone faster-rcnn
 # clone caffe
+git clone https://github.com/rbgirshick/py-faster-rcnn.git
 
-if [ ! -d $WORKSPACE/py-faster-rcnn ]
-then
-    git clone https://github.com/rbgirshick/py-faster-rcnn.git
-fi
-
-if [ ! -d $WORKSPACE/py-faster-rcnn/caffe-fast-rcnn ]
-then
-    cd $WORKSPACE/py-faster-rcnn
-    git clone https://github.com/BVLC/caffe.git ./caffe-fast-rcnn
-    cp $WORKSPACE/installation/pycaffe.Makefile.config ./py-faster-rcnn/caffe-fast-rcnn/Makefile.config
-fi
+cd $WORKSPACE/py-faster-rcnn
+git clone https://github.com/BVLC/caffe.git ./caffe-fast-rcnn
+cp $WORKSPACE/installation/pycaffe.Makefile.config $WORKSPACE/py-faster-rcnn/caffe-fast-rcnn/Makefile.config
 
 # install dependencies
 cd $WORKSPACE/py-faster-rcnn/caffe-fast-rcnn/python
-    # pip
-sudo pip install -r requirements.txt
-    # apt
-sudo apt-get install -y \
-    libprotobuf-dev \
-    libleveldb-dev \
-    libsnappy-dev \
-    libopencv-dev \
-    libhdf5-serial-dev \
-    protobuf-compiler\
-    libgflags-dev libgoogle-glog-dev liblmdb-dev \
-    libatlas-base-dev liblapack-dev && \
-    apt-get install --no-install-recommends libboost-all-dev -y && \
-# update configuration
-ldconfig
 
 # install libs
 cd $WORKSPACE/py-faster-rcnn/lib && \
