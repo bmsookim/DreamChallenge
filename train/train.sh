0c55d6a23ab9b34b4f44464a33ee7afa215b5193
bumsoo <meliketoy@gmail.com>
2016-12-14 16:53:29 +0900

    train script will be used

--- train/train.sh
+++ train/train.sh
@@ -1,10 +1,8 @@
 export netType='resnet'
 export depth=50
-export width=2
+export width=1
 export dataset='dreamChallenge'
 export data='/preprocessedData/dreamCh/'
-export save=logs/${netType}-${depth}x${width}/
-export experiment_number=1
 
 th main.lua \
 -dataset ${dataset} \
@@ -13,8 +11,8 @@ th main.lua \
 -nGPU 2 \
 -batchSize 32 \
 -dropout 0 \
--imageSize 224 \
--cropSize 224 \
+-imageSize 512 \
+-cropSize 512 \
 -top5_display false \
 -testOnly false \
 -resume '' \
