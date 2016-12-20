64e5fa3a2687ee14e20de2be8a10e1dff72eb52f
yuminhwan <minhwan90@gmail.com>
2016-12-20 16:24:32 +0900

    upload sc*_infer.sh

--- sc1_infer.sh
+++ sc1_infer.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+WORKDIR=$(pwd)
+
+# image preprocessing
+cd $WORKDIR/pre-processing
+python DREAM_DM_preprocessing.py --corpus dreamCh --dataset test --form robust --processor 4 --queue test --exams_metada 0
+
+# inference 
+cd $WORKDIR/train
+./test.sh
+
+# scoring
+cd $WORKDIR/test
+python score.py
