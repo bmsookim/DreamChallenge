f2fb4ae93827926cf95a3d1a0921fb0920816000
MinhwanYu-X1 <minhwan90@gmail.com>
2016-12-26 22:30:29 +0900

    pre-processin -> submodule"

--- pre-processing/Torch/preprocess.lua
+++
@@ -1,9 +0,0 @@
-require 'torch'
-require 'paths'
-require 'optim'
-require 'nn'
-
-local datasets = require 'datasets/init'
-local opts = require 'opts'
-local _opt = opts.parse(arg)
-datasets.generate(_opt)
