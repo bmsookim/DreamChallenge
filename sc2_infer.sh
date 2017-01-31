7b0f57a41ba2f2c19ea9a70ec536f9b701f4003f
yuminhwan <minhwan90@gmail.com>
2017-01-31 15:30:22 +0900

    update sc2_infer.sh

--- sc2_infer.sh
+++ sc2_infer.sh
@@ -4,5 +4,11 @@ WORKDIR=$(pwd)
 ls /modelState
 # inference 
 cd $WORKDIR/train
-python test.py -q test -c dreamCh -d test
-
+python test.py \
+    --config ../dicom-preprocessing/config/train.yaml   \
+    --corpus dreamCh    \
+    --dataset test      \
+    --exam_meta False   \
+    --processor 1       \
+    --log False         \
+    --form mem          
